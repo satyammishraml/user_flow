9b2e683a40af7442253f8489fbf1c8b3e0aa2e55
satyammishra <satyammishra@auw.co.in>
2022-05-02 11:56:17 +0530

    code

--- user_flow.py
+++ user_flow.py
@@ -28,52 +28,49 @@ def run_query(query):
     return data
 
 
-retentioneering.config.update({
-    'user_col': 'user_id',
-    'event_col': 'entity_type',
-    'event_time_col': 'timestamp',
-})
+with st.form(key='my_form'):
+    #user_id = st.text_input('Enter User ID', 'Enter User ID')
+    today = datetime.today()
+    tomorrow = today + timedelta(days=10)
+    start_date = st.date_input('Start date', today)
+    end_date = st.date_input('End date', tomorrow)
+    if start_date > end_date:
+        st.error('Error: End date must fall after start date.')
 
-#user_id = st.text_input('Enter User ID', 'Enter User ID')
-today = datetime.today()
-tomorrow = today + timedelta(days=10)
-start_date = st.date_input('Start date', today)
-end_date = st.date_input('End date', tomorrow)
-if start_date > end_date:
-    st.error('Error: End date must fall after start date.')
+    n = st.text_input('Enter Top n entities', 6)
+    submit_button = st.form_submit_button(label='Submit')
+    if submit_button:
+        start_date = start_date.strftime("%Y-%m-%d")
+        end_date = end_date.strftime("%Y-%m-%d")
+        events_views_query = make_event_query(start_date)
+        df = run_query(events_views_query)
 
-start_date = start_date.strftime("%Y-%m-%d")
-end_date = end_date.strftime("%Y-%m-%d")
-events_views_query = make_event_query(start_date)
-df = run_query(events_views_query)
+        
+        entity_types = df.entity_type.value_counts().reset_index()['index'][:int(n)].tolist()
+        short_df = df[df.entity_type.isin(entity_types)]
+        data = short_df[['user_id', 'entity_type', 'timestamp']]
 
+        st.write(short_df)
+        retentioneering.config.update({
+            'user_col': 'user_id',
+            'event_col': 'entity_type',
+            'event_time_col': 'timestamp',
+        })
 
-n = st.text_input('Enter Top n entities', 6)
-entity_types = df.entity_type.value_counts().reset_index()['index'][:int(n)].tolist()
-short_df = df[df.entity_type.isin(entity_types)]
-data = short_df[['user_id', 'entity_type', 'timestamp']]
-
-st.write(short_df)
-retentioneering.config.update({
-    'user_col': 'user_id',
-    'event_col': 'entity_type',
-    'event_time_col': 'timestamp',
-})
-
-data.rete.plot_graph(norm_type=None,
-                     weight_col=None,
-                     thresh=250)
-plt.show()
+        data.rete.plot_graph(norm_type=None,
+                             weight_col=None,
+                             thresh=250)
+        plt.show()
 
 
 
-import glob
-html_file = glob.glob('experiments/*.html', recursive=True)[-1]
-import streamlit.components.v1 as components
+        import glob
+        html_file = glob.glob('experiments/*.html', recursive=True)[-1]
+        import streamlit.components.v1 as components
 
-HtmlFile = open(html_file, 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-components.html(source_code, height = 1000)
+        HtmlFile = open(html_file, 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        components.html(source_code, height = 1000)
 
 
 
